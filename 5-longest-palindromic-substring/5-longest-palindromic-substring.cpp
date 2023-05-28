737f6f2d404e9c06b4d5981a587d567afd9dfd82
Anshu1Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-05-29 01:00:39 +0530

    Time: 328 ms (35.34%), Space: 342.4 MB (7.47%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string longestPalindrome(string str) {
+        int n=str.length();
+        int ind1=0,ind2=0;
+       vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+         dp[0][0]=1;
+        for(int i=1;i<n;i++){
+            if(str[i]==str[i-1]){
+                dp[i-1][i]=1;
+                ind1=i-1;
+                ind2=i;
+            }
+            dp[i][i]=1;
+        }
+       
+        for(int i=1;i<n;i++){
+           
+            int q=0;
+            int p=i;
+            while(p<n){
+                if(str[q]==str[p]&&dp[q+1][p-1]==1){
+                    dp[q][p]=1;
+                    ind1=q;
+                    ind2=p;
+                }
+                p++;
+                q++;
+            }
+        }
+        
+        if(str.size()==1)return str;
+          string ans;
+       if(ind1==-1||ind2==-1){
+           ans=ans+str[0];
+       }
+        for(int i=ind1;i<=ind2;i++){
+            ans=ans+str[i];
+        }
+        return ans;
+        
+        
+        
+    }
+};
