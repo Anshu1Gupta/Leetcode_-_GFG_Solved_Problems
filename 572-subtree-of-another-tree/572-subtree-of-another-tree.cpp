2090c178a8e8d01accb77bbd8a6f6a4c03cb664e
Anshu1Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-06-06 14:15:07 +0530

    Time: 24 ms (75.12%), Space: 28.8 MB (39.06%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -12,39 +12,33 @@
 class Solution {
 public:
     
-    bool check(TreeNode* root, TreeNode* subRoot){
-        if(root==NULL&&subRoot==NULL)return true;
-        if(subRoot==NULL||root==NULL)return false;
+   bool inorder(TreeNode* root, TreeNode* subRoot){
+        if(root==NULL)return false;;
+        bool left=inorder(root->left,subRoot);
+        bool right=inorder(root->right,subRoot);
+        // if(left||right)return true;
+        if(left||right)return true;
         
-        if(root->val!=subRoot->val)return false;
+        if(root->val==subRoot->val){
+            return check(root,subRoot);
+        }
+       
+       return false; 
+    }
+    bool check(TreeNode* root, TreeNode* subRoot){
+        
+        if(root==NULL&&subRoot!=NULL||subRoot==NULL&&root!=NULL)return false;
+        if(root==NULL&&subRoot==NULL)return true;
         
         bool left=check(root->left,subRoot->left);
         bool right=check(root->right,subRoot->right);
         
-        // if(left&&right&&(root->val==subRoot->val))return true;
-        return left&right;
+        if(left&&right&&(root->val==subRoot->val))return true;
+        return false;
     }
      
     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-       if(!root) return 0;
-        if(check(root, subRoot)) return 1;
-        bool left = isSubtree(root->left, subRoot);
-        bool right = isSubtree(root->right, subRoot);
-        return left|right;
-    }    
-    //  bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-    //     if(!root) return 0;
-    //     if(isSameTree(root, subRoot)) return 1;
-    //     bool left = isSubtree(root->left, subRoot);
-    //     bool right = isSubtree(root->right, subRoot);
-    //     return left|right;
-    // }
-    // bool isSameTree(TreeNode* p, TreeNode* q) {
-    //     if(!p && !q) return 1;
-    //     if(!p || !q) return 0;
-    //     if(p->val!=q->val) return 0;
-    //     bool left = isSameTree(p->left, q->left);
-    //     bool right = isSameTree(p->right, q->right);
-    //     return left&right;
-    // }
+        if(subRoot==NULL)return true;
+      return inorder(root,subRoot);
+    }
 };
