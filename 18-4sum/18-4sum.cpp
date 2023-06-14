3a7f6d8b0d87cc0b3c0cee9f6ee89035da8a119c
Anshu1Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-06-15 00:07:57 +0530

    Time: 45 ms (92.62%), Space: 13.1 MB (75.34%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
-        set<vector<int>>set;                                                        
+        vector<vector<int>>ans;                                                       
                                                                 
         for(int i=0;i<nums.size();i++){
             for(int j=i+1;j<nums.size();j++){
@@ -10,25 +10,24 @@ public:
                 int low=j+1,high=nums.size()-1;
                 while(low<high){
                     long long sum=(long long)(nums[low]+nums[high]);
-                    if(sum==remain){
-                        set.insert({nums[i],nums[j],nums[low],nums[high]});
-                        low++,high--;
-                    }
-                    else if(sum>remain){
-                        high--;
-                    }
+                    if(sum>remain)high--;
+                    else if(sum<remain)low++;
+                    
                     else{
-                        low++;
+                        ans.push_back({nums[i],nums[j],nums[low],nums[high]});
+                        
+                        int temp1=low,temp2=high;
+                        while(low<high&&nums[low]==nums[temp1])low++;
+                        while(low<high&&nums[temp2]==nums[high])high--;
                     }
                     
                 }
+            while(j<(nums.size()-1)&&nums[j]==nums[j+1])j++;
             }
+            while(i<(nums.size()-1)&&nums[i]==nums[i+1])i++;
         }
-            vector<vector<int>>ans;
-        for(auto it:set){
-            ans.push_back(it);
-        }
-                              
+           
+                      
      return ans;
                                                                 
     }
