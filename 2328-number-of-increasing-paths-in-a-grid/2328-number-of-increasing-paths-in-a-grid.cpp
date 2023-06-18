32b0ea22ec245b6b790a4ce02f62bf28911163d9
Anshu1Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-06-18 22:58:25 +0530

    Time: 317 ms (48.73%), Space: 43.1 MB (96.48%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -1,50 +1,48 @@
 class Solution {
 public:
     int m, n;
-    vector<vector<int>> directions = {
+    vector<pair<int,int>> dir = {
                 {-1,0},
         {0,-1},         {0,1},
                 {1, 0}
     };
     
-    int t[1001][1001];
+    int dp[1001][1001];
     
-    bool isSafe(int i, int j) {
-        return (i < m && i >= 0 && j < n && j >= 0);
-    }
+
     
     long long MOD = 1e9+7;
     
     int dfs(vector<vector<int>>& grid, int i, int j) {
         
-        if(t[i][j] != -1)
-            return t[i][j];
+        if(dp[i][j] != -1)
+            return dp[i][j];
         
         int answer = 1;
         
-        for(auto &dir : directions) {
-            int i_ = i + dir[0];
-            int j_ = j + dir[1];
+        for(auto &dir : dir) {
+            int i_ = i + dir.first;
+            int j_ = j + dir.second;
             
-            if(isSafe(i_, j_) && grid[i_][j_] < grid[i][j]) {
+            if(i_<n&&j_<m&&i_>=0&&j_>=0&& grid[i_][j_] < grid[i][j]) {
                 answer = (answer + dfs(grid, i_, j_)) % MOD;
             }
         }
         
-        return t[i][j] = answer;
+        return dp[i][j] = answer;
         
     }
     
     int countPaths(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
+        n = grid.size();
+        m = grid[0].size();
         
-        memset(t, -1, sizeof(t));
+        memset(dp, -1, sizeof(dp));
         
         int result = 0;
         
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
                 
                 result = (result + dfs(grid, i, j)) % MOD;
                 
