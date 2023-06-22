a28f13109275f8d38791a2554bcc914dc49af3fc
Anshu1Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-06-22 15:39:44 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (62.96%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-void findSet(int ind,vector<int>&temp,set<vector<int>>&ans,vector<int>&arr){
+void findSet(int ind,vector<int>&temp,vector<vector<int>>&ans,vector<int>&arr){
     if(ind>=arr.size()){
-        ans.insert(temp);
+        ans.push_back(temp);
         return;
     }
 
     temp.push_back(arr[ind]);
     findSet(ind+1,temp,ans,arr);
     temp.pop_back();
+    while(ind+1<arr.size()&&arr[ind+1]==arr[ind])ind++;
     findSet(ind+1,temp,ans,arr);
 }
     vector<vector<int>> subsetsWithDup(vector<int>& arr) {
-    set<vector<int>>ans;
+    sort(arr.begin(),arr.end());
+    vector<vector<int>>ans;
     vector<int>temp;
     findSet(0,temp,ans,arr);
-    vector<vector<int>>res;
-    set<vector<int>>ans1;
-    for(auto it:ans){
-        sort(it.begin(),it.end());
-        ans1.insert(it);
-    }
-        for(auto it:ans1){
-            res.push_back(it);
-        }
-    return res;
+    return ans;
     }
 };
