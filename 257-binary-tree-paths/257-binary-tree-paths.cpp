8bc2a713c174c8f206142cdf01398108c0da0b67
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-07-08 21:55:01 +0530

    Time: 0 ms (100.00%), Space: 13.3 MB (34.36%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -11,31 +11,29 @@
  */
 class Solution {
 public:
-  void  findTree(TreeNode*root,vector<string>&ans,string &temp){
-      if(root==NULL)return;  
-      if(root->left==NULL&&root->right==NULL){
-            string num=to_string(root->val);
-          int s=temp.size();
-            temp.append(num);
-            ans.push_back(temp);
-            while(temp.size()!=s)temp.pop_back();
-            return ;
-        }
-        int size=temp.size();
-        string num=to_string(root->val);
-        temp.append(num);
-        temp.append("->");
-        findTree(root->left,ans,temp);
-        findTree(root->right,ans,temp);
-        while(temp.size()!=size){
-            temp.pop_back();
-        }
-    }
+void f(TreeNode* root,string temp,vector<string> &ans)
+  {
+      temp=temp+to_string(root->val);
+
+      if(root->left==NULL && root->right==NULL)
+      {
+          ans.push_back(temp);
+      }
+        
+     if(root->left)
+      f(root->left,temp+"->",ans);
+     if(root->right)
+      f(root->right,temp+"->",ans);
+
+      temp.pop_back();
+  }
+
     vector<string> binaryTreePaths(TreeNode* root) {
         
         vector<string>ans;
-        string temp;
-        findTree(root,ans,temp);
-        return ans;
+        string temp="";
+        f(root,temp,ans);
+        return ans;
+
     }
 };
