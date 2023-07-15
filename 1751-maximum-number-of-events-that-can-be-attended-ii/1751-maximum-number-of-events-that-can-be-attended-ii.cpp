17337d7176effa9fad94d8c26198bb642dbc6667
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-07-15 13:36:18 +0530

    Time: 273 ms (97.95%), Space: 80.5 MB (51.82%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    int maxEvents(int ind, vector<pair<pair<int,int>,int>>&vect,int k,vector<vector<int>>&dp){
+    int maxEvents(int ind,int prev, vector<pair<pair<int,int>,int>>&vect,int k,vector<vector<int>>&dp){
         if(ind>=vect.size()||k<=0)return 0;
+        if(prev!=-1&& vect[ind].first.first <= vect[prev].first.second ) 
+              return maxEvents( ind + 1 ,prev,vect,k,dp );
         
         if(dp[ind][k]!=-1)return dp[ind][k];
-        int i;
-        for(i=ind+1;i<vect.size();i++){
-            if(vect[ind].first.second<vect[i].first.first)break;
+        int take=0,ntake=0;
+        if(prev==-1||vect[prev].first.second<vect[ind].first.first){
+            take=vect[ind].second+maxEvents(ind+1,ind,vect,k-1,dp);
         }
-        
-        return dp[ind][k]= max(maxEvents(ind+1,vect,k,dp),vect[ind].second+maxEvents(i,vect,k-1,dp));
-        
-        
-        
-        // int take=0;
-        // if(prev==-1||vect[prev].first.second<vect[ind].first.first){
-        //     take=vect[ind].second+maxEvents(ind+1,ind,vect,k-1,dp);
-        // }
-        // int ntake=0+maxEvents(ind+1,prev,vect,k,dp);
-        // return dp[ind][k]=max(take,ntake);
+    
+            ntake=0+maxEvents(ind+1,prev,vect,k,dp);
+       
+        return dp[ind][k]=max(take,ntake);
     }
     int maxValue(vector<vector<int>>& events, int k) {
         vector<pair<pair<int,int>,int>>vect;
@@ -28,6 +23,6 @@ public:
         sort(vect.begin(),vect.end());
         int n=vect.size();
         vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
-        return maxEvents(0,vect,k,dp);
+        return maxEvents(0,-1,vect,k,dp);
     }
 };
