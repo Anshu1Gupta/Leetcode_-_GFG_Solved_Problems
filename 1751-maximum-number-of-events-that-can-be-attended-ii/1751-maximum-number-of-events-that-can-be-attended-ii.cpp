7e38faabf9dec5faf9fdd326b99db70f0ed6c7f2
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-07-15 13:45:39 +0530

    Time: 266 ms (98.89%), Space: 80.6 MB (51.82%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
-    int maxEvents(int ind,int prev, vector<pair<pair<int,int>,int>>&vect,int k,vector<vector<int>>&dp){
+    int findSearch(int low, vector<pair<pair<int,int>,int>>&vect,int k){
+        int high=vect.size()-1;
+        int ind=vect.size();
+        while(low<=high){
+            int mid=(low+high)/2;
+            
+            if(vect[mid].first.first>k){
+                ind=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        
+       return ind;
+    }
+    int maxEvents(int ind, vector<pair<pair<int,int>,int>>&vect,int k,vector<vector<int>>&dp){
         if(ind>=vect.size()||k<=0)return 0;
-        if(prev!=-1&& vect[ind].first.first <= vect[prev].first.second ) 
-              return maxEvents( ind + 1 ,prev,vect,k,dp );
+        // if(prev!=-1&& vect[ind].first.first <= vect[prev].first.second ) 
+        //       return maxEvents( ind + 1 ,prev,vect,k,dp );
         
         if(dp[ind][k]!=-1)return dp[ind][k];
-        int take=0,ntake=0;
-        if(prev==-1||vect[prev].first.second<vect[ind].first.first){
-            take=vect[ind].second+maxEvents(ind+1,ind,vect,k-1,dp);
-        }
-    
-            ntake=0+maxEvents(ind+1,prev,vect,k,dp);
-       
-        return dp[ind][k]=max(take,ntake);
+        
+        // int take=0,ntake=0;
+        // if(prev==-1||vect[prev].first.second<vect[ind].first.first){
+        //     take=vect[ind].second+maxEvents(ind+1,ind,vect,k-1,dp);
+        // }
+        // ntake=0+maxEvents(ind+1,prev,vect,k,dp);
+        
+        int find=findSearch(ind+1,vect,vect[ind].first.second);
+        
+        
+        return dp[ind][k]=max(maxEvents(ind+1,vect,k,dp),vect[ind].second+maxEvents(find,vect,k-1,dp));
     }
     int maxValue(vector<vector<int>>& events, int k) {
         vector<pair<pair<int,int>,int>>vect;
@@ -23,6 +41,6 @@ public:
         sort(vect.begin(),vect.end());
         int n=vect.size();
         vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
-        return maxEvents(0,-1,vect,k,dp);
+        return maxEvents(0,vect,k,dp);
     }
 };
