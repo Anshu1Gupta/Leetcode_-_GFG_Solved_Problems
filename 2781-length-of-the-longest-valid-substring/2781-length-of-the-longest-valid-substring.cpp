cea964cbf9aef40ac03f75a22442c205f329f3f0
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-07-17 14:33:56 +0530

    Time: 576 ms (68.97%), Space: 122.5 MB (68.97%) - LeetHub

---
+++ 2781-length-of-the-longest-valid-substring/2781-length-of-the-longest-valid-substring.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int longestValidSubstring(string word, vector<string>& forbidden) {
+        unordered_set<string>st;
+        int maxLen=0;
+        for(auto it:forbidden){
+            st.insert(it);
+            int len=it.length();
+            maxLen=max(maxLen,len);
+        }
+        
+        
+        int ans=0,start=0,end=0,n=word.length();
+        
+        while(end<n){
+            
+            int i=end;
+            string temp;
+            for(;i>=start&&i>(end-maxLen);i--){
+                temp=word[i]+temp;
+                if(st.find(temp)!=st.end()){
+                    start=i+1;
+                    break;
+                }
+            }
+            
+            ans=max(ans,end-start+1);
+            end++;
+        }
+        return ans;
+    }
+};
