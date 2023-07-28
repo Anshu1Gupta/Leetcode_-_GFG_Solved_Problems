ebd86713de013387ac68a967412aae4010cd9375
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-07-28 08:49:13 +0530

    Time: 137 ms (30.99%), Space: 7.7 MB (29.30%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int findIt(int i,int j,vector<int>&nums,vector<vector<int>>&dp){
         if(i>j)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
+        // if(dp[i][j]!=-1)return dp[i][j];
         int sum1=nums[i]+min(findIt(i+2,j,nums,dp),findIt(i+1,j-1,nums,dp));
         int sum2=nums[j]+min(findIt(i+1,j-1,nums,dp),findIt(i,j-2,nums,dp));
         
-        return dp[i][j]=max(sum1,sum2);
+        return max(sum1,sum2);
         
     }
     
