e0a483e50b1d44db68ea6ce339469918de5455ac
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-08-05 17:55:59 +0530

    Added solution - LeetHub

--- M-Coloring Problem - GFG/mcoloring-problem.cpp
+++ M-Coloring Problem - GFG/mcoloring-problem.cpp
@@ -4,67 +4,34 @@ using namespace std;
 
 
 // } Driver Code Ends
-
-// class Solution{
-// public:
-
-//     bool isPlace (int source,vector<int>adj[],vector<int>&coloured,int col){
-//         for(auto it:adj[source]){
-//             if(coloured[it]==col)return false;
-//         }
-//         return true;
-//     }
-//     bool canColour(int source,vector<int>adj[],vector<int>&coloured,int m,int n ){
-//         if(source==n)return true;
-//         for(int i=1;i<=m;i++){
-//             if(isPlace(source,adj,coloured,i)==true){
-//                 coloured[source]=i;
-//                 if(canColour(source+1,adj,coloured,m,n))return true;
-//                 else coloured[source]=-1;
-//             }
-//         }
-//     }
-//     bool graphColoring(bool graph[101][101], int m, int n) {
-//         vector<int>adj[102];
-//         vector<int>coloured(102,-1);
-//         for(int i=0;i<102;i++){
-//             for(int j=0;j<102;j++){
-//                 if(graph[i][j]==1){
-//                     adj[i].push_back(j);
-//                     adj[j].push_back(i);
-//                 }
-//             }
-//         }
-        
-       
-//         if(canColour(0,adj,coloured,m,n)==true)return true;
-//         return false;
-//     }
-// };
 class Solution{
 public:
     // Function to determine if graph can be coloured with at most M colours such
     // that no two adjacent vertices of graph are coloured with same colour.
-    bool isPlace(vector<int>adj[],vector<int>&color,int node,int col){
+    bool canPos(int node,vector<int>adj[],vector<int>&color,int col){
         for(auto it:adj[node]){
             if(color[it]==col)return false;
         }
         return true;
     }
-    bool solve(vector<int>adj[],vector<int>&color,int node,int n,int m){
-        if(node==n)return true;
+    bool canColor(int node,vector<int>adj[],vector<int>&color,int m,int n){
+        if(node==n){
+            return true;
+        }
+        
         for(int i=1;i<=m;i++){
-            if(isPlace(adj,color,node,i)==true){
+            if(canPos(node,adj,color,i)){
                 color[node]=i;
-                if(solve(adj,color,node+1,n,m)==true)return true;
-                else color[node]=0;
+                if(canColor(node+1,adj,color,m,n))return true;
+                color[node]=-1;
             }
         }
         return false;
     }
     bool graphColoring(bool graph[101][101], int m, int n) {
         vector<int>adj[102];
-        vector<int>color(102,0);
+        vector<int>color(102,-1);
+        
         for(int i=0;i<102;i++){
             for(int j=0;j<102;j++){
                 if(graph[i][j]==1){
@@ -74,10 +41,7 @@ public:
             }
         }
         
-        if(solve(adj,color,0,n,m)==true)return true;
-        return false;
-        
-        
+        return canColor(0,adj,color,m,n);
     }
 };
 
