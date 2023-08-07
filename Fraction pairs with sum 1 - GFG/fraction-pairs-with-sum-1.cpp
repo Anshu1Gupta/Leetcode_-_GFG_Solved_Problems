d91af8adfe03e2650c3bc810c00a46ce1e8ea8aa
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-08-08 01:06:00 +0530

    Added solution - LeetHub

--- Fraction pairs with sum 1 - GFG/fraction-pairs-with-sum-1.cpp
+++ Fraction pairs with sum 1 - GFG/fraction-pairs-with-sum-1.cpp
@@ -12,17 +12,14 @@ class Solution
     {
         int count=0;
         unordered_map<string,int>mp;
+        string str,find;
         for(int i=0;i<n;i++){
             int gcd=__gcd(numerator[i],denominator[i]);
         numerator[i]=numerator[i]/gcd;
         denominator[i]=denominator[i]/gcd;
-     
         int num=denominator[i]-numerator[i];
-        
-        
-        string find=to_string(num)+"and"+to_string(denominator[i]);
-               
-        string str=to_string(numerator[i])+"and"+to_string(denominator[i]);
+        find=to_string(num)+"and"+to_string(denominator[i]);
+        str=to_string(numerator[i])+"and"+to_string(denominator[i]);
         count+=mp[find];
         mp[str]++;
         }
