ac9520594250d64a54bfe49dae7bea3214c79952
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-08-08 19:14:40 +0530

    Time: 331 ms (96.94%), Space: 89.7 MB (88.12%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -4,18 +4,23 @@ public:
         sort(points.begin(),points.end());
         
         vector<pair<int,int>>poi;
-        poi.push_back({points[0][0],points[0][1]});
-        int n=points.size();
+        int first=points[0][0];
+        int second=points[0][1];
         
+        
+        int n=points.size();
+        int count=0;
         for(int i=1;i<n;i++){
-            if(poi.back().second<points[i][0]){
-                poi.push_back({points[i][0],points[i][1]});
+            if(second<points[i][0]){
+                first=points[i][0];
+                second=points[i][1];
+                count++;
             }
             else{
-                poi.back().first=max(poi.back().first,points[i][0]);
-                poi.back().second=min(poi.back().second,points[i][1]);
+                first=max(first,points[i][0]);
+                second=min(second,points[i][1]);
             }
         }
-        return poi.size();
+        return count+1;
     }
 };
