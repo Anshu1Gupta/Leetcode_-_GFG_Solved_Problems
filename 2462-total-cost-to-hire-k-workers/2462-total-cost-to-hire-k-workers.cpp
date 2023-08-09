30e743f1db6c71e0b83ec880bff13808ae89c3de
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-08-09 12:22:55 +0530

    Time: 126 ms (99.31%), Space: 72.9 MB (32.08%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        priority_queue<int,vector<int>,greater<int>>front,back;
-        int i=0,j=costs.size()-1;
+        
+       priority_queue<int,vector<int>,greater<int>>front,back;
         long long sum=0;
-    
-        while(k!=0){
-            
-            while(front.size()<candidates&&(i<=j))front.push(costs[i++]);
-            while(back.size()<candidates&&(j>=i))back.push(costs[j--]);
-            
-            int fro=front.size()>0?front.top():INT_MAX;
-            int bac=back.size()>0?back.top():INT_MAX;
-            
-            if(fro<=bac){
-                sum+=fro;
-                front.pop();
-            }
-            else{
-                sum+=bac;
-                back.pop();
-            }
-                k--;
-            
-        }
+        int i=0,j=costs.size()-1;
+       while(k--){
+           while(front.size()<candidates&& i<=j)front.push(costs[i++]);
+           while(back.size()<candidates&& j>=i)back.push(costs[j--]);
+           
+         
+           int first=front.size()>0?front.top():INT_MAX;
+           int second=back.size()>0?back.top():INT_MAX;
+           if(first<=second){
+               sum+=first;
+               front.pop();
+           }
+           else{
+               sum+=second;
+               back.pop();
+           }
+       }
         return sum;
     }
-        
-        
 };
