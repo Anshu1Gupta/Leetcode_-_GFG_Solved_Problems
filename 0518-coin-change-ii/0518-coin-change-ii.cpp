df0d4620c0ed9d4542357a5c0d508e53030778ec
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-08-11 11:07:56 +0530

    Time: 30 ms (52.89%), Space: 18.3 MB (58.93%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -17,8 +17,27 @@ public:
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
         
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return nways(n-1,coins,amount,dp);
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        int target=amount;
+        // return nways(n-1,coins,amount,dp);
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0]==0){
+                dp[0][i]=1;
+            }
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=target;j++){
+                int take=0;
+                if(j>=coins[i]){
+                    take=dp[i][j-coins[i]];
+                }
+                int ntake=dp[i-1][j];
+                
+                dp[i][j]=take+ntake;
+            }
+        }
+        return dp[n-1][target];
         
     }
 };
