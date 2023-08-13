35c17e5f77b8837e171deac449053230ad125fa3
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-08-13 22:29:09 +0530

    Time: 171 ms (95.64%), Space: 92 MB (34.22%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<pair<int,int>>pair;
-        int n=nums2.size();
+        vector<pair<int,int>>dir;
+        int n=nums1.size();
+        for(int i=0;i<nums1.size();i++){
+            dir.push_back({nums2[i],nums1[i]});
+        }
+        
+        sort(dir.begin(),dir.end());
+        
         priority_queue<int,vector<int>,greater<int>>pq;
-        for(int i=0;i<n;i++){
-            pair.push_back({nums2[i],nums1[i]});
-        }
-        
-        sort(pair.begin(),pair.end());
-        long long ans=0;
-        long long  sum=0;
+        long long sum=0;
         for(int i=n-1;i>(n-1-k);i--){
-            pq.push(pair[i].second);
-            sum+=pair[i].second;
+            sum+=dir[i].second;
+            pq.push(dir[i].second);
         }
-       
-        long long val=sum*pair[n-k].first;
-        ans=max(ans,val);
-      
-        int end=n-1-k;
         
+        long long ans=0;
+        long long val=sum*dir[n-k].first;
+        
+        ans=max(ans,val);
+        int end=n-1-k;
         while(end>=0){
-            int ele=pq.top();pq.pop();
-            sum-=ele;
-            sum+=pair[end].second;
-            pq.push(pair[end].second);
-            
-            long long val=sum*pair[end].first;
+            int mini=pq.top();pq.pop();
+            sum-=mini;
+            sum+=dir[end].second;
+            val=sum*dir[end].first;
+            pq.push(dir[end].second);
             ans=max(ans,val);
-            
-            
             end--;
         }
         return ans;
-        
     }
 };
+
+// 1 2 3 4  5 6 7 8 9
+// 0 1 2 3  4 5 6 7 8
