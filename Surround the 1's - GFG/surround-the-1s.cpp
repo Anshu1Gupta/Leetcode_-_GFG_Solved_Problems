1268345949fab5337e2b0776f9da6ae17f3c95d9
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-08-21 00:25:20 +0530

    Added solution - LeetHub

--- Surround the 1's - GFG/surround-the-1s.cpp
+++ Surround the 1's - GFG/surround-the-1s.cpp
@@ -7,8 +7,9 @@ using namespace std;
 
 class Solution {
 public:
-vector<pair<int,int>>dir{{-1,0},{0,-1},{1,0},{0,1},{-1,-1},{1,1},{1,-1},{-1,1}};
     int Count(vector<vector<int> >& matrix) {
+        // Code here
+        vector<pair<int,int>>dir{{-1,0},{0,-1},{1,0},{0,1},{-1,-1},{1,1},{1,-1},{-1,1}};
        int n=matrix.size();
        int m=matrix[0].size();
        int count=0;
