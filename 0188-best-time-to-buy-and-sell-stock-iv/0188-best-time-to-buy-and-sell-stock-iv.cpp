ff742f10ad9e41628d9078c2d184962a67e4afca
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-08-23 22:17:53 +0530

    Time: 16 ms (66.48%), Space: 17.7 MB (9.28%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -42,19 +42,7 @@ public:
         }
         return dp[0][0][k];
 
-        // for(int i=n-1; i>=0; i--){
-        //     for(int j=0; j<2; j++){
-        //         for(int x=1; x<k+1; x++){
-        //             if(j==1){
-        //                 dp[i][j][x]=max((-1)*prices[i]+dp[i+1][0][x], dp[i+1][j][x]);
-        //             }
-        //             else{
-        //                 dp[i][j][x]=max(prices[i]+dp[i+1][1][x-1], dp[i+1][j][x]);
-        //             }
-        //         }
-        //     }
-        // }
-        // return dp[0][1][k];
+      
 
     }
 };
