beed483ba559fd947780a86d7ad484a29320a2e8
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-08-28 18:53:06 +0530

    Time: 187 ms (9.34%), Space: 27.9 MB (13.39%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool ifMatched(int ind1,int ind2,string &str1,string &str2,vector<vector<int>>&dp){
+       if(ind1<0&&ind2<0)return true;
+       if(ind2<0)return false;
+       if(ind1<0){
+           for(int i=0;i<=ind2;i++){
+               if(str2[i]!='*')return false;
+           }
+           return true;
+       } 
+        if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
+        
+        
+        if(str1[ind1]==str2[ind2]||str2[ind2]=='?'){
+            return dp[ind1][ind2]=ifMatched(ind1-1,ind2-1,str1,str2,dp);
+        }
+        
+        if(str2[ind2]=='*'){
+            for(int i=ind1;i>=-1;i--){
+                if(ifMatched(i,ind2-1,str1,str2,dp))return dp[ind1][ind2]=true;
+            }
+        }
+        
+        return dp[ind1][ind2]=false;
+        
+    }
+    bool isMatch(string s, string p) {
+        int n=s.length();
+        int m=p.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return ifMatched(n-1,m-1,s,p,dp);
+    }
+};
