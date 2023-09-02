18761b8b8864cfb5319393111ac01d69d110c1fa
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-09-03 00:49:47 +0530

    Added solution - LeetHub

--- Check if Tree is Isomorphic - GFG/check-if-tree-is-isomorphic.cpp
+++ Check if Tree is Isomorphic - GFG/check-if-tree-is-isomorphic.cpp
@@ -92,7 +92,8 @@ struct Node {
 
 class Solution{
   public:
-   bool toCheck(Node*root1,Node*root2){
+    // Return True if the given trees are isomotphic. Else return False.
+    bool toCheck(Node*root1,Node*root2){
      if(root1==NULL&&root2==NULL)return true;
      if(root1==NULL||root2==NULL)return false;
      if(root1->data!=root2->data)return false;
