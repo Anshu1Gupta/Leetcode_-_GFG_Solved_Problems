121455e98e27593d7eac2abb2dad72f3df9aa26f
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-09-11 00:33:08 +0530

    Time: 137 ms (93.25%), Space: 69.8 MB (44.65%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums) {
-        int n=nums.size();
-        set<int>set;
-        for(auto it:nums){
-            set.insert(it);
-        }
-        int maxi=0;
-        vector<int>ans;
-        for(auto it:set){
-            ans.push_back(it);
-        }
-        sort(ans.begin(),ans.end());
-        
-        int right=0;
-        
-        int nS=ans.size();
-        
-        for(int left=0;left<n;left++){
-            while(right<nS&&(ans[right]-ans[left]<=n-1))right++;
-            
-            int curr=right-left;
-            maxi=max(maxi,curr);
-        }
-        return n-maxi;
-    }
+	int minOperations(vector<int>& nums) {
+   	 vector<int>ans;
+       sort(nums.begin(),nums.end());
+       for(int i=0;i<nums.size();i++){
+         if(ans.size()==0||ans.back()!=nums[i]){
+           ans.push_back(nums[i]);
+         }
+       }
+       int right=0;
+       int n=ans.size();
+       int maxi=0;
+       for(int left=0;left<n;left++){
+           while(right<n&&(ans[right]-ans[left]<=(nums.size()-1))){
+              right++;
+           }
+           int window_size=right-left;
+           maxi=max(maxi,window_size);
+       }
+
+
+       return nums.size()-maxi;
+
+	}
 };
+
+
