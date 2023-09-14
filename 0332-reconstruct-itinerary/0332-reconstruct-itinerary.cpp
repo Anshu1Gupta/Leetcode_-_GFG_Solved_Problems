def50ab089a7b714ffad9d73ea78a4555bea5b98
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-09-14 18:04:31 +0530

    Time: 15 ms (89.66%), Space: 16.2 MB (17.15%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     vector<string>ans;
-    
     bool dfs(string source,unordered_map<string,vector<string>>&adj,vector<string>&path,int tic){
         path.push_back(source);
-        
         if(path.size()==tic+1){
             ans=path;
             return true;
         }
-        
         vector<string>&neighbours=adj[source];
-        
         for(int i=0;i<neighbours.size();i++){
             string des=neighbours[i];
             neighbours.erase(neighbours.begin()+i);
@@ -23,22 +19,18 @@ public:
     }
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         unordered_map<string,vector<string>>adj;
-        
         for(auto it:tickets){
             string from=it[0];
             string to=it[1];
-            
             adj[from].push_back(to);
         }
         for(auto &it:adj){
             sort(it.second.begin(),it.second.end());
         }
-        
         string source="JFK";
         vector<string>path;
         int tic=tickets.size();
         if(dfs(source,adj,path,tic))return ans;
         return {};
-        
     }
 };
