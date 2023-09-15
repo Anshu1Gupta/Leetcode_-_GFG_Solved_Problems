543ca9cd1e1427705301fd4fe028dccb638a30b5
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-09-15 17:31:33 +0530

    Time: 2157 ms (5.38%), Space: 535.8 MB (5.01%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,59 +1,45 @@
-class DisjointSet{
-    public:
-    vector<int>parent,size;
-    public:
-    DisjointSet(int n){
-        parent.resize(n+1),size.resize(n+1);
-        for(int i=0;i<=n;i++){
-            size[i]=1;
-            parent[i]=i;
-        }
-    }
-    
-    int findUParent(int node){
-        if(node==parent[node])return parent[node];
-        return parent[node]=findUParent(parent[node]);
-    }
-    
-    void unionBySize(int u,int v){
-        int ult_u=findUParent(u);
-        int ult_v=findUParent(v);
-        
-        if(ult_u==ult_v)return ;
-        
-        if(size[ult_u]>size[ult_v]){
-            size[ult_u]+=size[ult_v];
-            parent[ult_v]=ult_u;
-        }
-        else{
-            size[ult_v]+=size[ult_u];
-            parent[ult_u]=ult_v;
-        }
-    }
-};
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        vector<vector<int>>edges;
         int n=points.size();
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+        vector<vector<int>>adj[n];
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                edges.push_back({abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]),i,j});
+                int u=i;
+                int v=j;
+                int distance=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+                
+                adj[u].push_back({v,distance});
+                adj[v].push_back({u,distance});
             }
         }
-        DisjointSet ds(n);
+        
+        
+        q.push({0,0});
         int sum=0;
-        sort(edges.begin(),edges.end());
-        for(auto it:edges){
-            int u=it[1];
-            int v=it[2];
-            int dist=it[0];
+        vector<int>visited(n+1,0);
+        
+        
+        while(!q.empty()){
+            int node=q.top().second;
+            int dist=q.top().first;
             
-            if(ds.findUParent(u)!=ds.findUParent(v)){
-                ds.unionBySize(u,v);
-                sum+=dist;
+            q.pop();
+            if(visited[node]==1)continue;
+            
+            visited[node]=1;
+            sum+=dist;
+            for(auto v:adj[node]){
+                 int ad=v[0];
+                 int di=v[1];
+                if(!visited[ad]){
+                    q.push({di,ad});
+                }
             }
         }
+        
         return sum;
     }
+    
 };
