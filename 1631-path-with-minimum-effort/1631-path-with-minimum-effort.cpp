97b358d2b860cd5f22de93107f3443a9213530e7
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-09-16 11:50:59 +0530

    Time: 204 ms (27.46%), Space: 31.9 MB (27.99%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
-   vector<pair<int,int>>dir{{-1,0},{0,-1},{1,0},{0,1}};
-int minimumEffortPath(vector<vector<int>>& heights) {
-int row=heights.size();
-int col=heights[0].size();
- 
-   vector<vector<int>>visited(row, vector<int>(col ,1e9));
-   priority_queue<pair<int,pair<int,int>> ,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>q;
-  
-  q.push({0,{0,0}});
-  // int diff=0;
-  visited[0][0]=0;
-  while(!q.empty()){
-  int effort=q.top().first;
-  int r=q.top().second.first;
-  int c=q.top().second.second;
-    q.pop();
-    
-    if(r==row-1&&c==col-1)return effort;
-    for(auto it:dir){
-      int nx=r+it.first;
-      int ny=c+it.second;
-      if(nx>=0&&nx<row && ny>=0&&ny<col){
-        int newEffort=abs(heights[nx][ny]-heights[r][c]);
-        newEffort=max(effort,newEffort);
-          if(newEffort<visited[nx][ny]){
-               q.push({newEffort,{nx,ny}});
-               visited[nx][ny]=newEffort;
-          }
-       
-      }
+    vector<pair<int,int>>dir{{-1,0},{0,-1},{0,1},{1,0}};
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n=heights.size();
+        int m=heights[0].size();
+        vector<vector<int>>effort(n,vector<int>(m,1e9));
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        effort[0][0]=0;
+        while(!q.empty()){
+            int x=q.front().first;
+            int y=q.front().second;
+            q.pop();
+            for(auto it:dir){
+                int nx=x+it.first;
+                int ny=y+it.second;
+                if(nx>=0&&nx<n&&ny>=0&&ny<m){
+                    int newEffort=abs(heights[nx][ny]-abs(heights[x][y]));
+                    int val=max(newEffort,effort[x][y]);
+                    
+                    if(effort[nx][ny]>val){
+                        effort[nx][ny]=val;
+                        q.push({nx,ny});
+                    }
+                }
+            }
+        }
+        
+        return effort[n-1][m-1];
     }
-  }
-
-
-return 0;
-   	 
-}
-
 };
