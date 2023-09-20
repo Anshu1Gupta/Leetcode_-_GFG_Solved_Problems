65858b16519bdc371e085de4532f3abe56f2fefd
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-09-20 12:53:15 +0530

    Time: 3 ms (41.32%), Space: 7.2 MB (6.00%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -6,13 +6,11 @@ public:
         for(int i=1;i<numRows;i++){
             vector<int>temp;
             for(int j=0;j<=i;j++){
-                if(j==0)temp.push_back(ans[i-1][0]);
+                if(j==0)temp.push_back(ans[i-1][j]);
                 else if(j==i)temp.push_back(ans[i-1][j-1]);
-                
                 else{
                     int left=ans[i-1][j];
                     int right=ans[i-1][j-1];
-    
                     temp.push_back(left+right);
                 }
             }
