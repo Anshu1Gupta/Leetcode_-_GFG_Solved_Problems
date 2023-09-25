b4e81eeed01e909a039c18755f5ac1b9d8224945
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-09-26 00:56:01 +0530

    Added solution - LeetHub

---
+++ Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+    public:
+    // arr[] : int input array of integers
+    // k : the quadruple sum required
+    vector<vector<int> > fourSum(vector<int> &arr, int k) {
+        // Your code goes here
+        int n=arr.size();
+        vector<vector<int>>ans;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int low=j+1;
+                int high=n-1;
+                
+                while(low<high){
+                    int sum=arr[i]+arr[j]+arr[low]+arr[high];
+                    
+                    if(sum<k){
+                        low++;
+                    }
+                    else if(sum>k){
+                        high--;
+                    }
+                    else{
+                        ans.push_back({arr[i],arr[j],arr[low],arr[high]});
+                        
+                        int ind1=low;
+                        while(low<high&&arr[low]==arr[ind1])low++;
+                        ind1=high;
+                        while(low<high&&arr[high]==arr[ind1])high--;
+                    }
+                }
+            
+            while(j<(arr.size()-1)&&arr[j]==arr[j+1])j++;
+            }
+            while(i<(arr.size()-1)&&arr[i]==arr[i+1])i++;
+            
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k, i;
+        cin >> n >> k;
+        vector<int> a(n);
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        vector<vector<int> > ans = ob.fourSum(a, k);
+        for (auto &v : ans) {
+            for (int &u : v) {
+                cout << u << " ";
+            }
+            cout << "$";
+        }
+        if (ans.empty()) {
+            cout << -1;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
