cb3f55ed4a0e1a51e95a50d9e1e0ef10c6c283f7
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-10-01 11:00:42 +0530

    Time: 13 ms (74.16%), Space: 10 MB (55.04%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+bool space(char c){
+    if(c==' ')return true;
+    else return false;
+}
+
+    string reverseWords(string s) {
+        int start=0;
+        int i=0;
+        for(;i<s.length();i++){
+            if(space(s[i])){
+                reverse(s.begin()+start,s.begin()+i);
+                start=i+1;
+            }
+        }
+        reverse(s.begin()+start,s.begin()+i);
+         return s;
+    }
+   
+};
