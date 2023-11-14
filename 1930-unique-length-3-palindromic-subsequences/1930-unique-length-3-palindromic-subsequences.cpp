ebe00f613cc063ad5c0a63efd2c89e6f255ce98a
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2023-11-14 19:07:10 +0530

    Time: 245 ms (78.31%), Space: 13.4 MB (65.98%) - LeetHub

---
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,28 @@
+
+
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        int res = 0;
+        std::unordered_set<char> uniq;
+
+        for (char c : s) {
+            uniq.insert(c);
+        }
+
+        for (char c : uniq) {
+            int start = s.find(c);
+            int end = s.rfind(c);
+
+            if (start < end) {
+                std::unordered_set<char> charSet;
+                for (int i = start + 1; i < end; i++) {
+                    charSet.insert(s[i]);
+                }
+                res += charSet.size();
+            }
+        }
+
+        return res;        
+    }
+};
