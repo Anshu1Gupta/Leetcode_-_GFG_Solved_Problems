18896b8a3348304ea04086700ba5de32c981e88f
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2024-06-16 13:48:12 +0530

    Time: 36 ms (34.81%), Space: 21.4 MB (30.19%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    bool canEat(int capacity,vector<int>&piles,int maxHour){
-        
+    bool findFind(vector<int>&nums,int capacity,int h){
         int initialHour=0;
-        for(int i=0;i<piles.size();i++){
-            int t=ceil(piles[i]/(capacity*1.0));
+        
+       for(int i=0;i<nums.size();i++){
+            int t=ceil(nums[i]/(capacity*1.0));
             initialHour+=t;
-            if(initialHour>maxHour)return false;
+            if(initialHour>h)return false;
         }
         return true;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low=1;
-        int high=*max_element(piles.begin(),piles.end());
-        
-        
-        
+      int low=1;
+      int high=*max_element(piles.begin(),piles.end());
         int ans=h;
+        int mid=-1;
         while(low<=high){
-            int mid=(low+high)/2;
-            
-            
-            if(canEat(mid,piles,h)){
+            int k=(low+high)/2;
+            mid=k;
+            if(findFind(piles,k,h)){
                 ans=mid;
-                high=mid-1;
+                high=k-1;
             }
             else{
-                low=mid+1;
+                low=k+1;
+             
             }
         }
-        
         return ans;
-        
     }
 };
