54d0583c3f128d9a0efcd79e7d01a059747e1788
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2024-06-22 19:13:15 +0530

    Time: 104 ms (30.36%), Space: 70.1 MB (36.89%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<int>arr(n);
-        
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==1)arr[i]=1;
-            else arr[i]=0;
-        }
-        
-        unordered_map<int,int>mp;
-        mp[0]=1;
-        
-        int rsum=0;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            rsum+=arr[i];
+        int ansCnt = 0; // Final count of subarrays
+        int cnt = 0;    // Count of valid subarrays ending at current position
+        int i = 0, j = 0; // Two pointers for the sliding window
+
+        while (j < nums.size()) {
+            if (nums[j] % 2 != 0) { // If the current element is odd
+                k--; // Decrement k as we have encountered an odd number
+                cnt = 0; // Reset the count of valid subarrays ending at this position
+            }
             
-            int diff=rsum-k;
-            ans+=mp[diff];
+            while (k == 0) { // While we have exactly k odd numbers in the window
+                if (nums[i] % 2 != 0) { // If the leftmost element of the window is odd
+                    k++; // Increment k as we will remove this odd number
+                }
+                cnt++; // Increment count of valid subarrays ending at current position
+                i++; // Move the left pointer of the window to the right
+            }
             
-            mp[rsum]++;
+            ansCnt += cnt; // Add the count of valid subarrays to the final answer
+            j++; // Move the right pointer of the window to the right
         }
-        return ans;
+
+        return ansCnt; // Return the final count of subarrays with exactly k odd numbers
     }
 };
