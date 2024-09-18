ddb4fb8d0edcaa03fcc5dca834e62219796a0ef1
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2024-09-18 22:46:43 +0530

    Time: 3 ms (84.52%), Space: 16.2 MB (91.37%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        vector<pair<int,int>>dir{{-1,0},{0,-1},{1,0},{0,1}};
+      queue<pair<int,int>>q;
+      vector<pair<int,int>>dir{{-1,0},{0,-1},{1,0},{0,1}};
         int n=grid.size();
         int m=grid[0].size();
-        queue<pair<int,int>>q;
+        
+        
         int fresh=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==2){
                     q.push({i,j});
                 }
-                else if(grid[i][j]==1){
-                    fresh++;
-                }
+                if(grid[i][j]==1)fresh++;
             }
         }
-        
         if(fresh==0)return 0;
-        if(q.empty()&&fresh!=0)return -1;
-        
-        int count=1;
+        int time=0;
         while(!q.empty()){
-            int s=q.size();
             
-            while(s--){
+            int size=q.size();
+            while(size--){
                 int x=q.front().first;
                 int y=q.front().second;
                 q.pop();
@@ -32,19 +29,19 @@ public:
                     int nx=x+it.first;
                     int ny=y+it.second;
                     
-                    if(nx>=0&&nx<n&&ny>=0&&ny<m&&grid[nx][ny]==1){
+                    if(nx>=0 && ny>=0 &&nx<n &&ny<m && grid[nx][ny]==1){
                         fresh--;
-                        if(fresh==0)return count;
-                        
-                        grid[nx][ny]=2;
                         q.push({nx,ny});
-                        
+                        grid[nx][ny]=2;
                     }
                 }
             }
-            count++;
+          time++;
+            
         }
-        return -1;
         
+        if(fresh!=0)return -1;
+        
+        return time-1;
     }
 };
