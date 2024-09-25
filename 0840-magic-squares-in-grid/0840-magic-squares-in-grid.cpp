e0fee7038981dc7ed969aa389d794406cf215205
Anshu Gupta <116982173+Anshu1Gupta@users.noreply.github.com>
2024-09-25 18:49:20 +0530

    Time: 6 ms (22.63%), Space: 11 MB (52.93%) - LeetHub

---
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+// total sum of a magic grid will always be 45
+// therefore sum of each row, column, diagonal will be 15 
+// there are limited formations such that the sum of each row, column and diagonal is 15
+// there will be 8 such formations and the formations will have 2 things in common 
+// the middle element is always 5 and rest elements are a subset of  org or revformat(check code)
+     bool isValid(int i, int j, vector<vector<int>>& grid) {
+        vector<int> count(10, 0);
+        for (int x = 0; x < 3; ++x) {
+            for (int y = 0; y < 3; ++y) {
+                int num = grid[i + x][j + y];
+                if (num < 1 || num > 9 || count[num] > 0) return false;
+                count[num]++;
+            }
+        }
+
+        int sum = grid[i][j] + grid[i][j+1] + grid[i][j+2];
+        
+        for(int x = 0; x < 3; ++x) {
+            if(sum != (grid[i + x][j] + grid[i + x][j + 1] + grid[i + x][j + 2])) return false;
+        }
+        
+        for(int y = 0; y < 3; ++y) {
+            if(sum != (grid[i][j + y] + grid[i + 1][j + y] + grid[i + 2][j + y])) return false;
+        }
+        
+        if(sum != (grid[i][j] + grid[i+1][j+1] + grid[i+2][j+2])) return false;
+        if(sum != (grid[i+2][j] + grid[i+1][j+1] + grid[i][j+2])) return false;
+
+        return true;
+    }
+    
+public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int cnt = 0;
+        int row = grid.size();
+        int col = grid[0].size();
+        
+        if(row < 3 || col < 3) return 0;
+
+        for(int i = 0; i <= row - 3; ++i) {
+            for(int j = 0; j <= col - 3; ++j) {
+                if(isValid(i, j, grid)) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
